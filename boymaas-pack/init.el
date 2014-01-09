fb445f727b6a261a9de7ac8490ef0c669d079d56
Boy Maas <boy.maas@gmail.com>
2014-01-09 20:21:47 +0100

    Init working emacs live config

---
+++ boymaas-pack/init.el
@@ -0,0 +1,11 @@
+;; User pack init file
+;;
+;; Use this file to initiate the pack configuration.
+;; See README for more information.
+
+;; Load bindings config
+(live-load-config-file "bindings.el")
+
+;; byte compile everything that needs to be compiled for
+;; faster startup
+;;(byte-recompile-directory (expand-file-name "~/.emacs.d") 0)
